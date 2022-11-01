c12d1707d16b41400466865d5c89b9d4b09e4833
andrebruelementary <andre.brunsal@elementary-software.com>
2022-11-01 09:26:20 +0100

    Added support for saving DIDs to storage

--- popup.js
+++ popup.js
@@ -2,16 +2,38 @@ document.addEventListener('DOMContentLoaded', function() {
     var didbutton = document.getElementById('new_did_button');
     // onClick's logic below:
     didbutton.addEventListener('click', function() {
-        //alert('xxx');
-        chrome.runtime.sendMessage({msg: "The message sent from popup.js"},
-            function(response) {
-                // called when background responds
-                alert("popup received '"+ response.msg +"' from background");
+        chrome.storage.local.get('did_seed', function(result) {
+            //alert(JSON.stringify(result))
+            if(result['did_seed'] == undefined) {
+                //alert('No DID exists in storage. Request DID from web service');
+                chrome.runtime.sendMessage({msg: "The message sent from popup.js"}, function(response) {
+                    // called when background responds
+                    newDID = JSON.parse(response.msg);
+                    Object.keys(newDID).forEach(property => {
+                        var didProperty = {};
+                        didProperty[property] = newDID[property]; 
+                        chrome.storage.local.set(didProperty , function() {
+                            //alert(`${property} saved to Chrome local storage`);
+                        });
+                    });
+
+                    // TODO: make this aware of the async nature of setting properties
+                    alert("New DID saved in storage");
+                });
             }
-        );
+            else {
+                alert('DID found in storage');
+            }
+        });
     });
 });
 
-/*document.querySelector('new_did_button').addEventListener('click', function() {
-    console.log('Hi!');
-});*/
+document.addEventListener('DOMContentLoaded', function() {
+    var deletebutton = document.getElementById('delete_did_button');
+    // onClick's logic below:
+    deletebutton.addEventListener('click', function() {
+        //chrome.storage.sync.clear();
+        chrome.storage.local.remove(["did_seed", "did_canonical", "did_longform"]);
+        alert("DID deleted from storage");
+    });
+});
